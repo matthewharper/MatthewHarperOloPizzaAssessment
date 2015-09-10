f7d5a6e999b263f416a867ebed1f08a1461e3589
matthewharper <matthew.harper@yahoo.com>
2015-09-09 20:07:44 -0400

    Initial source code load.

---
+++ MatthewHarperOloPizzaAssessment/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace MatthewHarperOloPizzaAssessment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pizzaGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataUrlTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pizzaGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fetch Order Details";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pizzaGrid
+            // 
+            this.pizzaGrid.AllowUserToAddRows = false;
+            this.pizzaGrid.AllowUserToDeleteRows = false;
+            this.pizzaGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pizzaGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.pizzaGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pizzaGrid.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pizzaGrid.Location = new System.Drawing.Point(14, 136);
+            this.pizzaGrid.Name = "pizzaGrid";
+            this.pizzaGrid.ReadOnly = true;
+            this.pizzaGrid.Size = new System.Drawing.Size(1078, 527);
+            this.pizzaGrid.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Order History Details Grouped by Toppings";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(300, 53);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(96, 13);
+            this.statusLabel.TabIndex = 3;
+            this.statusLabel.Text = "Current Status: ";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.status.Location = new System.Drawing.Point(403, 53);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(38, 13);
+            this.status.TabIndex = 4;
+            this.status.Text = "Ready";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pizza Data URL:";
+            // 
+            // dataUrlTextBox
+            // 
+            this.dataUrlTextBox.Location = new System.Drawing.Point(407, 101);
+            this.dataUrlTextBox.Name = "dataUrlTextBox";
+            this.dataUrlTextBox.Size = new System.Drawing.Size(357, 20);
+            this.dataUrlTextBox.TabIndex = 6;
+            this.dataUrlTextBox.Text = "http://files.olo.com/pizzas.json";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 675);
+            this.Controls.Add(this.dataUrlTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pizzaGrid);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Matthew Harper\'s Olo Pizza Assessment";
+            ((System.ComponentModel.ISupportInitialize)(this.pizzaGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView pizzaGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dataUrlTextBox;
+    }
+}
+
---
+++ MatthewHarperOloPizzaAssessment/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace MatthewHarperOloPizzaAssessment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.pizzaGrid.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.pizzaGrid_RowPostPaint);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                status.Text = "... getting order data." ;
+                status.Refresh();
+                showTop20OrderDetails();
+                status.Text = "... showing order details.";
+                status.Refresh();
+            }
+            catch (Exception ex)
+            {
+                status.Text = "Error!";
+                //mjh:  Only performing elementary error handling for Assessment.
+                MessageBox.Show(@"An unexpected error occurred accessing or processing the JSON data.
+
+Please check that the data exists and that it is formatted correctly.
+
+Error details are below: 
+
+" + ex.ToString());
+            }
+        }
+
+        //mjh:  In a real project, this would be broken out appropriately.  
+        //      Keeping all logic here so it is easier for you to read.
+        //      Would put URL to data in config or db in real code.
+        private void showTop20OrderDetails()
+        {
+
+            string json;
+
+            // Get the data from the endpoint.
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString(dataUrlTextBox.Text);
+            }
+            
+            DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(List<Pizza>));
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            var orderHistory = (List<Pizza>)dcjs.ReadObject(ms);
+
+            PizzaComparer pc = new PizzaComparer();
+
+            // Put the data in the correct format.
+            var g = orderHistory
+                .GroupBy(p => p, pc)
+                .Select(p => new
+                {
+                    ConfigurationID = p.Key.GetHashCode(),
+                    ConfigurationCount = p.Count(),
+                    ConfigurationToppings = string.Join(",", p.Key.toppings)
+                }
+
+                )
+                .OrderByDescending(p => p.ConfigurationCount)
+                .Take(20);
+
+            // Show your stuff.
+            pizzaGrid.DataSource = g.ToList();
+            
+        }
+
+        // I didn't realize line numbers were not native to the DataGridView control.
+        // I do more programming in the web world, but this routine is pretty solid.
+        private void pizzaGrid_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            using (SolidBrush b = new SolidBrush(pizzaGrid.RowHeadersDefaultCellStyle.ForeColor))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font, b, e.RowBounds.Location.X + 10, e.RowBounds.Location.Y + 4);
+            }
+        }
+    }
+
+    // Something to work with.
+    [DataContract]
+    public class Pizza
+    {
+        [DataMember]
+        public string[] toppings { get; set; }
+    }
+
+
+    // Ensure we are comparing values for equals and not the object reference.
+    public class PizzaComparer : IEqualityComparer<Pizza>
+    {
+        public bool Equals(Pizza x, Pizza y)
+        {
+            return x.toppings.SequenceEqual(y.toppings);
+        }
+
+        public int GetHashCode(Pizza obj)
+        {
+            //mjh: Hack to generate unique id.  This may break if the order is different but the values are the same.
+            //      (i.e. {"pepperoni", "mushroom"} should equal {"mushroom", "pepperoni"})
+            //      I'm a little short on time to test.  SequenceEqual may have taken care of that since it compares
+            //      the values of the data instead of an object compare which would be a reference compare.
+            return (String.Join("|", obj.toppings)).GetHashCode();
+        }
+    }
+}
+
+
+
+
